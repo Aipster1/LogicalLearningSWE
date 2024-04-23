3c874e3a188dc95aa160ef068488ecc339739bc4
Aipster1 <bruecknerpia314@gmail.com>
2024-04-23 11:21:03 +0200

    Unity Commit
    First Unity Commit

---
+++ LogicalLearningSWE/Assets/ScrMainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ScrMainMenu : MonoBehaviour
+{
+
+public class ClickExample : MonoBehaviour {
+	public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+        SceneManager.LoadScene("MainMap");
+
+
+	}
+}
+}
